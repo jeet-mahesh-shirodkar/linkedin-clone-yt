8892fc99d1167e19e620b56b7748fed5a26479c7
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-06-15 20:12:32 +0530

    headerOption Component

---
+++ src/HeaderOption.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./HeaderOption.css";
+
+function HeaderOption({title,Icon}) {
+  return (
+    <div className='headerOption'>
+        {Icon && <Icon className = "headerOption__Icon"/>}
+        <h3 className='headerOption__title'>{title}</h3>
+    </div>
+  )
+}
+
+export default HeaderOption;
