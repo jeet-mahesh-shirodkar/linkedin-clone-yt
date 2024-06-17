023d58307bf5e22f8f4cf8f5c6b202341d1ac5ec
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-06-18 03:18:49 +0530

    Material UI Icons added

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./HeaderOption.css";
+import { Avatar } from '@mui/material';
 
-function HeaderOption({title,Icon}) {
+function HeaderOption({avator,title,Icon}) {
   return (
     <div className='headerOption'>
-        {Icon && <Icon className = "headerOption__Icon"/>}
+        {Icon && <Icon className = "headerOption__icon"/>}
+        {avator && <Avatar className='headerOption__icon' src={avator}/>}
         <h3 className='headerOption__title'>{title}</h3>
     </div>
   )
