656aac52b425aba2602e1a95e915a23c768ee4b6
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-06-19 02:16:13 +0530

    feed sidebar component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
     <div className="App">
       {/*Header*/}
-      <Header/>
+        <Header/>
       {/*App Body*/}
       <div className="app__body">
-      {/*Sidebar*/}
-        <Sidebar/>
-      {/*Feed*/}
-      {/*Widgets*/}
+        {/*Sidebar*/}
+          <Sidebar/>
+        {/*Feed*/}
+          <Feed/>
+        {/*Widgets*/}
       </div>
     </div>
   );
