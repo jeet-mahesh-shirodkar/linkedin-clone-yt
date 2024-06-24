a11137d6aa4f3c051dcdd9a54eb0c487681365e9
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-06-25 00:57:50 +0530

    Fetch docs and render post

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
@@ -7,17 +7,56 @@ import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import Post from './Posts';
+import {db} from "./firebase";
+import 'firebase/compat/firestore';
+import { serverTimestamp } from 'firebase/firestore';
 
 
 function Feed() {
+  
+  const [input,setInput] = useState('');  
+  const [posts,setPosts] = useState([]);
+  
+  useEffect(() => {
+    
+    db.collection("posts")
+      .orderBy("timestamp","desc")
+      .onSnapshot((snapshot) =>
+        {
+            setPosts(
+                snapshot.docs.map((doc)=>({
+                        id:doc.id,
+                        data:doc.data()
+                }))
+            )
+        }    
+    )
+    },[]);
+
+  let submitPost = (e) => {
+    e.preventDefault();
+
+    db.collection("posts")
+      .add({
+        name:'Jeet',
+        description:"This is test",
+        messages:input,
+        photoUrl:'',
+        //timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        timestamp: serverTimestamp(),
+        });
+
+        setInput("");
+  }
+  
   return (
     <div className="feed">
         <div className="feed__inputContainer">
             <div className="feed__input">
                 <CreateIcon/>
                 <form>
-                    <input type='text'/>
-                    <button type='submit'>Send</button>
+                    <input  value={input} onChange={(e) =>{setInput(e.target.value)}} type='text'/>
+                    <button onClick={submitPost} type='submit'>Send</button>
                 </form>
             </div>
             <div className="feed__inputOptions">
@@ -31,8 +70,18 @@ function Feed() {
                               color={"#7FC15E"}/>
             </div>
         </div>
+        { posts.map(({id,data:{name,description,messages,photoUrl,timestamp}})=>{
+            return(
+                <Post   
+                key={id}
+                name={name} 
+                description={description}
+                messages={messages}
+                photoURL={photoUrl}
+                />
+            )
+        })}
         
-        <Post name={'Jeet'} description={'This is test'} messages={'This is a test message'}/>
     </div>
   )
 }
