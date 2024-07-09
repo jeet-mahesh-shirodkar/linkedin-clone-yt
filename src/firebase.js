968045b106f5f959bca96569264ab260d00061cf
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2024-07-10 03:02:17 +0530

    Add compat firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 //import firebase from "firebase/app";
 //import 'firebase/database';
 import firebase from 'firebase/compat/app';
